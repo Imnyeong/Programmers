2577bfc4996f35de268fda185eb3c790dfbffdad
Imnyeong <lmh4873@gmail.com>
2022-10-18 23:33:54 +0900

    2022.10.18

---
+++ Level_1/x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public long[] solution(int x, int n)
+    {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; ++i)
+        {
+            answer[i] = (long)x * (i + 1);
+        }
+        // 규칙대로 배열에 대입 long 배열에 int가 껴서 그런지 (long) 체크가 필요  
+        return answer;
+    }
+}
